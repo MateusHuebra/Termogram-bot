8f3d37ce9827af67fc7e22d1e92203f8e7cda4b2
mateushuebra <mateushuebra@gmail.com>
2022-03-22 11:49:34 -0300

    added PalavrecoController

---
+++ app/Http/Controllers/PalavrecoController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commands\Factory;
+use App\Services\ServerLog;
+use Illuminate\Http\Request;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Types\Update;
+
+class PalavrecoController extends Controller
+{
+    
+    function listen(Request $request) {
+        ServerLog::log('BotController > listen');
+        $bot = new \TelegramBot\Api\Client(env('TG_TOKEN'));
+        $update = Update::fromResponse(BotApi::jsonValidate($bot->getRawBody(), true));
+
+        $command = Factory::buildCommand($update, $bot);
+        if($command) {
+            $command->run($update, $bot);
+        }
+
+    }
+
+}
+
+        //$bot->getChe
+
+        /*
+        $bot->on(function (Update $update) use ($bot) {
+            $bot->sendMessage($update->getMessage()->getChat()->getId(), 'pong');
+        }, function() {
+            return true;
+        });
+        */
+        
+        //$bot->sendMessage(, 'test');
+
+
+        /*
+        try {
+            $bot = new \TelegramBot\Api\Client(env('TG_TOKEN'));
+            
+            $bot->command('ping', function ($message) use ($bot) {
+                $bot->sendMessage($message->getChat()->getId(), 'pong!', $message->getId());
+            });
+            
+            $bot->run();
+        } catch (\TelegramBot\Api\Exception $e) {
+            $e->getMessage();
+        }
+        */
