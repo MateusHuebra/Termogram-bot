c76355419c9316f3d767a6348c9ccc64888acc40
mateushuebra <mateushuebra@gmail.com>
2022-03-22 13:49:09 -0300

    added help string

--- app/Commands/Help.php
+++ app/Commands/Help.php
@@ -2,11 +2,13 @@
 
 namespace App\Commands;
 
+use App\Services\TextString;
+
 class Help extends Command {
 
     public function run($update, $bot) {
         $userId = $this->getUserId($update);
-        $bot->sendMessage($userId, 'help');
+        $bot->sendMessage($userId, TextString::get('settings.help'));
     }
 
 }
