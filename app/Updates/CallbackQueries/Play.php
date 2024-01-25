c4b435f0b15926758898c22760ca195f8ae53a43
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-01-25 12:23:12 -0300

    make play by notification run

--- app/Updates/CallbackQueries/Play.php
+++ app/Updates/CallbackQueries/Play.php
@@ -5,14 +5,14 @@ namespace App\Updates\CallbackQueries;
 use App\Models\User;
 use App\Services\ServerLog;
 use App\Services\TextString;
-use App\Updates\Commands\Notifications;
+use App\Updates\Commands\Start;
 
 class Play extends CallbackQuery {
 
     public function run() {
         ServerLog::log('Open Notification > run');
-
-        
+        $start = new Start($update, $bot);
+        $start->run();
     }
 
 }
