da4b0d7ef5a56a40bcc246dfa8b7e12d484ba788
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-01-25 17:55:18 -0300

    minor fix to log

--- app/Services/ServerLog.php
+++ app/Services/ServerLog.php
@@ -5,7 +5,11 @@ namespace App\Services;
 class ServerLog {
 
     static function log($value) {
-        file_put_contents(storage_path('logs/logs.log'), "\n {$value}\n");
+        file_put_contents(
+            storage_path('logs/logs.log'),
+            "\n {$value}\n",
+            FILE_APPEND
+        );
     }
 
     static function printR($value) {
