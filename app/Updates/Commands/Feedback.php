b3af47dfb7343d2a7521bf56cfd470a32abdfd5d
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-03-17 09:57:26 -0300

    small fix

--- app/Updates/Commands/Feedback.php
+++ app/Updates/Commands/Feedback.php
@@ -23,15 +23,15 @@ class Feedback extends Command {
         ServerLog::log('Feedback > finished');
     }
 
-    private function getMessage() {
+    protected function getMessage() {
         return str_ireplace('/feedback ', '', $this->update->getMessage()->getText());
     }
 
-    private function formatMessage(string $message) {
+    protected function formatMessage(string $message) {
         return "\#feedback: [{$this->getUserName()}](tg://user?id={$this->getUserId()}) \({$this->getUserId()}:{$this->getMessageId()}\):\n```\n{$message}```";
     }
 
-    private function tryToSendMessage($message, $replyMessageId = null) {
+    protected function tryToSendMessage($message, $replyMessageId = null) {
         $userId = env('TG_MYID');
         try {
             ServerLog::log('trying to message '.$userId, false);
