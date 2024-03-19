833c545a205ca4c4f5e0ae7236dc63da41755320
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-03-19 01:12:12 -0300

    minor fix test

--- app/Updates/CallbackQueries/Play.php
+++ app/Updates/CallbackQueries/Play.php
@@ -6,12 +6,17 @@ use App\Models\User;
 use App\Services\ServerLog;
 use App\Services\TextString;
 use App\Updates\Commands\Start;
+use Exception;
 
 class Play extends CallbackQuery {
 
     public function run() {
         ServerLog::log('Play by Notification > run');
-        $this->bot->answerCallbackQuery($this->getId(), TextString::get('settings.loading'));
+        try {
+            $this->bot->answerCallbackQuery($this->getId(), TextString::get('settings.loading'));
+        } catch(Exception $e) {
+            //
+        }
         $start = new Start($this->update, $this->bot, true);
         $start->chatType = $this->getChatType();
         $start->userId = $this->getUserId();
