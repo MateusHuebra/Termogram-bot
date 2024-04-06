2eb7eda24b6735cf91f1265c2c4ea5d6b669a0ce
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-06 01:11:17 -0300

    minor changes 5

--- app/Updates/Commands/SecretImage.php
+++ app/Updates/Commands/SecretImage.php
@@ -13,8 +13,8 @@ class SecretImage extends Command {
         //$photo = new CURLFile(storage_path('verdant.jpg'),'image/jpg','testpic'); // uncomment and use if the upper procedural method is not working.
 
         // Cria uma imagem com largura e altura específicas
-        $largura = 1010;
-        $altura = 910;
+        $largura = 510;
+        $altura = 310;
         $imagem = imagecreatetruecolor($largura, $altura);
 
         $card = imagecreate(640, 140);
@@ -29,7 +29,7 @@ class SecretImage extends Command {
         imagefttext($card, 20, 0, 10, 25, $cor_fundo, storage_path('Cat Comic.ttf'), '1');
         imagefttext($card2, 25, 0, 10, 130, $cor_fundo, storage_path('Cat Comic.ttf'), $this->getFirstName());
         imagefttext($card2, 20, 0, 10, 25, $cor_fundo, storage_path('Cat Comic.ttf'), '2');
-        imagefttext($card3, 20, 0, 10, 130, $cor_fundo, storage_path('Cat Comic.ttf'), date('h:i:s'));
+        imagefttext($card3, 20, 0, 10, 130, $cor_fundo, storage_path('Cat Comic.ttf'), date('H:i:s'));
         imagefttext($card3, 20, 0, 10, 25, $cor_fundo, storage_path('Cat Comic.ttf'), '3');
 
         // Define a cor de fundo (branco)
@@ -42,32 +42,10 @@ class SecretImage extends Command {
         imagecopy($imagem, $card, 10, 10, 0, 0, 190, 140);
         imagecopy($imagem, $card2, 210, 10, 0, 0, 190, 140);
         imagecopy($imagem, $card3, 410, 10, 0, 0, 190, 140);
-        imagefilledrectangle($imagem, 610, 10, 800, 150, $cor_cartao);
-        imagefilledrectangle($imagem, 810, 10, 1000, 150, $cor_cartao);
 
         imagefilledrectangle($imagem, 10, 160, 200, 300, $cor_cartao);
         imagefilledrectangle($imagem, 210, 160, 400, 300, $cor_cartao);
         imagefilledrectangle($imagem, 410, 160, 600, 300, $cor_cartao);
-        imagefilledrectangle($imagem, 610, 160, 800, 300, $cor_cartao);
-        imagefilledrectangle($imagem, 810, 160, 1000, 300, $cor_cartao);
-
-        imagefilledrectangle($imagem, 10, 310, 200, 500, $cor_cartao);
-        imagefilledrectangle($imagem, 210, 310, 400, 500, $cor_cartao);
-        imagefilledrectangle($imagem, 410, 310, 600, 400, $cor_cartao);
-        imagefilledrectangle($imagem, 610, 310, 800, 500, $cor_cartao);
-        imagefilledrectangle($imagem, 810, 310, 1000, 500, $cor_cartao);
-
-        imagefilledrectangle($imagem, 10, 510, 200, 700, $cor_cartao);
-        imagefilledrectangle($imagem, 210, 510, 400, 700, $cor_cartao);
-        imagefilledrectangle($imagem, 410, 510, 600, 700, $cor_cartao);
-        imagefilledrectangle($imagem, 610, 510, 800, 700, $cor_cartao);
-        imagefilledrectangle($imagem, 810, 510, 1000, 700, $cor_cartao);
-
-        imagefilledrectangle($imagem, 10, 710, 200, 900, $cor_cartao);
-        imagefilledrectangle($imagem, 210, 710, 400, 900, $cor_cartao);
-        imagefilledrectangle($imagem, 410, 710, 600, 900, $cor_cartao);
-        imagefilledrectangle($imagem, 610, 710, 800, 900, $cor_cartao);
-        imagefilledrectangle($imagem, 810, 710, 1000, 900, $cor_cartao);
 
         //$gato = imagecreatefromjpeg(storage_path('endeota.jpg'));
         //imagecopy($imagem, $gato, 300, 300, 0, 0, 640, 640);
