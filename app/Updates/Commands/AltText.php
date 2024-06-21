23184d8d5869f1477c0593b04cbd293de2700764
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-06-21 13:04:50 -0300

    minor fix

--- app/Updates/Commands/AltText.php
+++ app/Updates/Commands/AltText.php
@@ -7,7 +7,7 @@ use App\Services\TextString;
 class AltText extends Command {
 
     public function run() {
-        $this->dieIfUnallowedChatType(['group', 'supergroup']);
+        $this->dieIfUnallowedChatType(['private']);
 
         $user = User::find($this->getUserId());
         $user->first_name = mb_substr($this->getFirstName(), 0, 16);
