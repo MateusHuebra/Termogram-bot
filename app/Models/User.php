a2e8075756a66634ee18983257e0969cd87e5d81
mateus <mateushuebra@gmail.com>
2025-02-22 10:23:05 -0300

    added hasMany games to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -13,6 +14,11 @@ class User extends Model
 
     public function scopeOfId($query, $userId) {
         return $query->where('id', $userId);
+    }
+    
+    public function games(): HasMany
+    {
+        return $this->hasMany(Game::class);
     }
 
 }
